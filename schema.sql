32e1f1c69009d8163c3dbc5509c8ea545b8c6d68
Liz <hyemlee@berkeley.edu>
2016-10-24 10:54:19 -0700

    Monday

--- schema.sql
+++ schema.sql
@@ -3,22 +3,24 @@
 drop table if exists travelers;
 create table travelers (
 	traveler_id integer primary key,
-	name text,
-	email text not null
+	traveler_name text not null
 );
 
-drop table if exists names;
+/*drop table if exists names;
 create table names (
 	name_id integer primary key,
 	name text,
 	travel_id integer,
 	FOREIGN KEY (name) REFERENCES travelers(name),
 	FOREIGN KEY (travel_id) REFERENCES travels(travel_id)
-);
+);*/
 
 drop table if exists travels;
 create table travels (
 	travel_id integer primary key,
 	trip_name text not null,
-	destination text not null
+	destination text not null,
+	friend text,
+	traveler_name text,
+	FOREIGN KEY (traveler_name) REFERENCES travelers(traveler_name)
 );
